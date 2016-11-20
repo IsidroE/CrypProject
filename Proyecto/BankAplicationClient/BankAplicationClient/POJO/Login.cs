0b535cd04a82cf29372e36651f9dd2119c8b61d6
IsidroE <chiloc5r@gmail.com>
2016-11-19 21:33:34 -0600

    Cambio 2
    agrego POJO login

---
+++ Proyecto/BankAplicationClient/BankAplicationClient/POJO/Login.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BankAplicationClient.POJO
+{
+    public class Login
+    {
+        private int idCuenta;
+        private string pasword;
+
+        public void setIdCuenta(int idCuenta)
+        {
+            this.idCuenta = idCuenta;
+        }
+        public int getIdCuenta()
+        {
+            return idCuenta;
+        }
+
+        public void setPasword(string pasword)
+        {
+            this.pasword = pasword;
+        }
+        public string getPasword()
+        {
+            return pasword;
+        }
+
+        public String toString()
+        {
+            return "Login{" + "idCuenta=" + idCuenta + ", pasword=" + pasword + '}';
+        }
+    }
+}
